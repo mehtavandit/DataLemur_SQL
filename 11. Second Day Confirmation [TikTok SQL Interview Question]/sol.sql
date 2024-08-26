dc97adfce5ef138d7e96004d530c14501578de49
mehtavandit <vanditmehta303@gmail.com>
2024-08-26 14:39:37 -0400

    here: https://datalemur.com/questions/second-day-confirmation

---
+++ 11. Second Day Confirmation [TikTok SQL Interview Question]/sol.sql
@@ -0,0 +1,5 @@
+SELECT DISTINCT user_id FROM emails
+INNER JOIN texts
+ON emails.email_id = texts.email_id
+where texts.action_date = emails.signup_date + INTERVAL '1 day'
+AND texts.signup_action = 'Confirmed';
